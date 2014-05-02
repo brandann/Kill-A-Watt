a50eb5b5afef27d8f44d434c409a8fd17346bbb4
Brandan Haertel <foilpudding@gmail.com>
2014-05-02 12:35:50 -0700

    added prototype
    Added Network Prototype

---
+++ prototype/NetworkPrototype/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    //The offset of the camera to centrate the player in the X axis
+    public float offsetX = 5;
+    public float rotationX = 5;
+    //The offset of the camera to centrate the player in the Z axis
+    public float offsetZ = -5;
+
+    public float offsetY = 10;
+    //The maximum distance permited to the camera to be far from the player, its used to     make a smooth movement
+    public float maximumDistance = 15;
+    //The velocity of your player, used to determine que speed of the camera
+    public float playerVelocity = 10;
+
+
+    private float movementX;
+    private float movementZ;
+    private float movementY;
+    private float lookDownAngle = 25;
+    void Start()
+    {
+        this.transform.rotation = Quaternion.Euler(lookDownAngle, 0, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementX = ((player.transform.position.x + offsetX - this.transform.position.x)) / maximumDistance;
+        movementZ = ((player.transform.position.z + offsetZ - this.transform.position.z)) / maximumDistance;
+        movementY = ((player.transform.position.y + offsetY - this.transform.position.y)) / maximumDistance;
+        //movementX = player.transform.position.x + offsetX / maximumDistance;
+        //movementY = player.transform.position.y + offsetY / maximumDistance;
+        //movementZ = player.transform.position.z + offsetZ / maximumDistance;
+        this.transform.position += new Vector3(movementX, movementY, movementZ);
+    }
+}
---
+++ prototype/NetworkPrototype/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private const string typeName = "NetworkPrototype";
+    private const string gameName = "SomeRoom";
+    private HostData[] hostList;
+    public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void StartServer()
+{
+    Network.InitializeServer(4, 25000, !Network.HavePublicAddress());
+    MasterServer.RegisterHost(typeName, gameName);
+}
+    void OnServerInitialized()
+    {
+        SpawnPlayer();
+    }
+
+    void OnGUI()
+    {
+        if (!Network.isClient && !Network.isServer)
+        {
+            if (GUI.Button(new Rect(0, 0, 250, 100), "Start Server"))
+                StartServer();
+
+            if (GUI.Button(new Rect(275, 0, 250, 100), "Refresh Hosts"))
+                RefreshHostList();
+
+            if (hostList != null)
+            {
+                for (int i = 0; i < hostList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(275, 100 + (110 * i), 250, 100), hostList[i].gameName))
+                        JoinServer(hostList[i]);
+                }
+            }
+        }
+    }
+
+   
+    private void RefreshHostList()
+    {
+        MasterServer.RequestHostList(typeName);
+    }
+
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        if (msEvent == MasterServerEvent.HostListReceived)
+            hostList = MasterServer.PollHostList();
+    }
+
+    private void JoinServer(HostData hostData)
+    {
+        Network.Connect(hostData);
+    }
+
+    void OnConnectedToServer()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        Network.Instantiate(playerPrefab, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+    }
+}
+
+ 
