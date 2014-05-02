ca5c1a07735593b283dec6773df301bf9beebbce
Brandan Haertel <foilpudding@gmail.com>
2014-05-02 15:15:49 -0700

    added sphere

---
+++ prototype/NetworkPrototype/Assets/SphereBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		#region user movement control
+		transform.position += Input.GetAxis ("Vertical")  * transform.up * (1 * Time.smoothDeltaTime);
+		transform.Rotate(Vector3.forward, -1f * Input.GetAxis("Horizontal") * (1 * Time.smoothDeltaTime));
+		#endregion
+	}
+}
