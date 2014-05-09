5d93fa49fda8018a1f31511299ddfb6c70408b85
erickhouse1 <erickhouse01@gmail.com>
2014-05-08 17:46:51 -0700

    addedmyscene

---
+++ prototype/animationProto/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//In C# this is the part that confused me most too.
+//	
+//	But, correct way of using find and getcomponent.
+//		
+//		GameObject.Find("ObjectName").GetComponent<ScriptN ame>()
+//		
+//		GameObject Object1 = GameObject.Find("name")
+//		Component Script1 = Object1.GetComponent<ScriptName>()
+//		
+//		Calling a function within the script: Script1.FunctionName()
+
+//GlobalBehavior globalBehavior = GameObject.Find ("Manager").GetComponent<GlobalBehavior>();
+
+namespace Global{
+
+public class GUIManager : MonoBehaviour {
+
+		gameManager Mscript;
+		WorldGameState GUIstatus;
+
+	void Start () {
+			 GameObject manager = GameObject.Find ("GameManager");
+			 Mscript = manager.GetComponent<gameManager>();
+			 GUIstatus =  Mscript.status;
+
+	}
+
+	void OnGUI() {
+
+	
+		
+		
+		switch (GUIstatus) 
+		{
+
+			case WorldGameState.SplashScreen:
+				GUI.Box(new Rect(0,0,100,90), "Test1");
+					break;
+
+			case WorldGameState.StartMenu:
+				GUI.Box(new Rect(5,5,100,90), "Test");
+					break;
+
+			case WorldGameState.InGame:
+					break;
+
+			case WorldGameState.EndGame:
+					break;
+
+		}
+
+
+
+//			if (status == WorldGameState.SplashScreen) {
+//				status = WorldGameState.StartMenu;
+//				
+//				print ("worked");
+//			}
+	}
+}
+
+}
---
+++ prototype/animationProto/Assets/Scripts/gameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Global{
+
+
+	public enum WorldGameState {
+		SplashScreen,
+		//possible story before start menu that can be quit out of 
+		StartMenu,
+		InGame,
+		EndGame
+		//possible stat screen at the end,
+	}
+
+	public class gameManager : MonoBehaviour {
+
+
+	//player 1
+	//player 2
+	// networking passed to networkManager?
+	// global score
+	// current game state
+	// switch game state function
+
+
+	//player class is a container class for all the objects manager needs to know about
+	// ID?
+	// their score
+	// number of towers they have
+	// number of minions?
+	 private player player1;
+	 private player player2;
+	 private Camera MainCamera;
+	 public WorldGameState status;
+
+
+	// this controls camera pos and passes the state to the GUI manager
+
+	void Start () {
+		status = WorldGameState.StartMenu;
+		player1 = new player();
+		player2 = new player();
+		MainCamera = Camera.main;
+	}
+	
+
+	void Update () {
+
+			switch (status) 
+			{
+				
+			case WorldGameState.SplashScreen:
+				break;
+				
+			case WorldGameState.StartMenu:
+				break;
+				
+			case WorldGameState.InGame:
+				break;
+				
+			case WorldGameState.EndGame:
+				break;
+				
+			}
+	
+	}
+
+
+}
+
+}
